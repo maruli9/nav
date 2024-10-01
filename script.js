3368a08295d21615739652e3a0fd9abd926755c3
maruli9 <118851819+maruli9@users.noreply.github.com>
2024-10-02 00:06:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const x = document.getElementById("myAudio"); 
+
+        function playAudio() { 
+        x.play(); 
+} 
+
+let menuList = document.getElementById("menuList")
+        menuList.style.maxHeight = "0px";
+
+        function toggleMenu(){
+            if(menuList.style.maxHeight == "0px")
+            {
+                menuList.style.maxHeight = "300px";
+            }
+            else{
+                menuList.style.maxHeight = "0px";
+            }
+        }
