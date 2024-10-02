61cf163df0f07a930f71725915cda764100d6baa
maruli9 <118851819+maruli9@users.noreply.github.com>
2024-10-02 11:48:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ const x = document.getElementById("myAudio");
 } 
 
 let menuList = document.getElementById("menuList")
+let cancel = document.getElementById('cancel')
+let burger = document.getElementById('burger')
         menuList.style.maxHeight = "0px";
 
         function toggleMenu(){
             if(menuList.style.maxHeight == "0px")
             {
                 menuList.style.maxHeight = "300px";
+                burger.style.display = 'none' 
+                cancel.style.display = 'block'
             }
             else{
                 menuList.style.maxHeight = "0px";
+                burger.style.display = 'block' 
+                cancel.style.display = 'none'
+
             }
         }
